7a63c8248432a08d32c7d261b67ce77588645647
renanlimaleite <renan.lima.softwares@gmail.com>
2022-06-23 15:06:00 -0300

    add: day three

--- event-platform/src/components/Lesson.tsx
+++ event-platform/src/components/Lesson.tsx
@@ -1,6 +1,7 @@
 import { format, isPast } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
 import { CheckCircle, Lock } from 'phosphor-react'
+import { Link } from 'react-router-dom'
 
 interface LessonProps {
   title: string
@@ -20,10 +21,10 @@ export function Lesson({ title, slug, availableAt, type }: LessonProps) {
   )
 
   return (
-    <a href="#">
+    <Link to={`/event/lesson/${slug}`} className="group">
       <span className="text-gray-300">{availableDateFormatted}</span>
 
-      <div className="rounded border border-gray-500 p-4 mt-2">
+      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
             <span className="flex items-center gap-2 text-sm text-blue-500 font-medium">
@@ -42,6 +43,6 @@ export function Lesson({ title, slug, availableAt, type }: LessonProps) {
         </header>
         <strong className="text-gray-200 block mt-5">{title}</strong>
       </div>
-    </a>
+    </Link>
   )
 }
