d45a782944f2bd0c287a86c10a644cdeadbee04f
renanlimaleite <renan.lima.softwares@gmail.com>
2022-06-25 13:14:20 -0300

    add default lesson in slug

--- event-platform/src/pages/Event.tsx
+++ event-platform/src/pages/Event.tsx
@@ -2,13 +2,18 @@ import { useParams } from 'react-router-dom'
 import { Header } from '../components/Header'
 import { Sidebar } from '../components/Sidebar'
 import { Video } from '../components/Video'
+import { useGetLessonsQuery } from '../graphql/generated'
 
 type Params = {
   slug: string
 }
 
 export function Event() {
-  const { slug } = useParams<Params>()
+  const { data } = useGetLessonsQuery()
+
+  const firstLesson = data?.lessons[0]?.slug
+
+  const { slug = firstLesson } = useParams<Params>()
 
   return (
     <div className="flex flex-col min-h-screen">
