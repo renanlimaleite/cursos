f5578b3bbd56de6d2047014e494dd8016ddd61d6
renanlimaleite <renan.lima.softwares@gmail.com>
2022-08-15 11:04:54 -0300

    eslint

--- ignite-timer/src/App.tsx
+++ ignite-timer/src/App.tsx
@@ -1,15 +1,15 @@
-import { ThemeProvider } from "styled-components";
-import { Button } from "./Button";
-import { GlobalStyle } from "./styles/global";
-import { defaultTheme } from "./styles/themes/default";
+import { ThemeProvider } from 'styled-components'
+import { Button } from './Button'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary"/>
-      <Button variant="danger"/>
-      <Button variant="secondary"/>
-      <Button variant="success"/>
+      <Button variant="primary" />
+      <Button variant="danger" />
+      <Button variant="secondary" />
+      <Button variant="success" />
       <GlobalStyle />
     </ThemeProvider>
   )
--- ignite-timer/src/Button.styles.ts
+++ ignite-timer/src/Button.styles.ts
@@ -8,15 +8,21 @@ const buttonVariants = {
   primary: 'purple',
   secondary: 'orange',
   danger: 'red',
-  success: 'green'
+  success: 'green',
 }
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
 
-  background-color: ${props => props.theme.primary};
-  /* ${props => {
-    return css`background-color: ${buttonVariants[props.variant]}`
+  background-color: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme.white};
+  /* ${(props) => {
+    return css`
+      background-color: ${buttonVariants[props.variant]};
+    `
   }} */
 `
