7c369bfedacef3fc118e1fd8a262e6a7d8e4b0c9
renanlimaleite <renan.lima.softwares@gmail.com>
2022-08-16 14:43:12 -0300

    add header e react router 6

--- ignite-timer/src/App.tsx
+++ ignite-timer/src/App.tsx
@@ -1,15 +1,15 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './Button'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="danger" />
-      <Button variant="secondary" />
-      <Button variant="success" />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
--- ignite-timer/src/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainer } from './Button.styles'
-
-interface ButtonProps {
-  variant?: 'primary' | 'secondary' | 'danger' | 'success'
-}
-
-export function Button({ variant = 'primary' }: ButtonProps) {
-  return <ButtonContainer variant={variant}>Enviar</ButtonContainer>
-}
