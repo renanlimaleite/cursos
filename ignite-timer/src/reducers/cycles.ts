210e5713eb1aba2e47e7b35013c92451b8792222
renanlimaleite <renan.lima.softwares@gmail.com>
2022-08-26 23:31:56 -0300

    add actions types and type action

---
+++ ignite-timer/src/reducers/cycles.ts
@@ -0,0 +1,74 @@
+export interface Cycle {
+  id: string
+  task: string
+  minutes: number
+  startDate: Date
+  interruptedDate?: Date
+  finishedDate?: Date
+}
+
+interface CyclesState {
+  cycles: Cycle[]
+  activeCycleId: string | null
+}
+
+export const ActionTypes = {
+  ADD_NEW_CYCLE: 'ADD_NEW_CYCLE',
+  INTERRUPT_CURRENT_CYCLE: 'INTERRUPT_CURRENT_CYCLE',
+  MARK_CURRENT_CYCLE_AS_FINISHED: 'MARK_CURRENT_CYCLE_AS_FINISHED',
+} as const
+
+type Action = {
+  type:
+    | 'INTERRUPT_CURRENT_CYCLE'
+    | 'ADD_NEW_CYCLE'
+    | 'MARK_CURRENT_CYCLE_AS_FINISHED'
+  payload?: {
+    newCycle?: Cycle
+    activeCycleId?: string | null
+  }
+}
+
+export function cyclesReducer(state: CyclesState, action: Action): CyclesState {
+  switch (action.type) {
+    case ActionTypes.ADD_NEW_CYCLE: {
+      if (action.payload?.newCycle) {
+        return {
+          ...state,
+          cycles: [...state.cycles, action.payload?.newCycle],
+          activeCycleId: action.payload?.newCycle?.id,
+        }
+      }
+      return state
+    }
+    case ActionTypes.INTERRUPT_CURRENT_CYCLE: {
+      return {
+        ...state,
+        cycles: state.cycles.map((cycle) => {
+          if (cycle.id === state.activeCycleId) {
+            return { ...cycle, interruptedDate: new Date() }
+          } else {
+            return cycle
+          }
+        }),
+        activeCycleId: null,
+      }
+    }
+    case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED: {
+      return {
+        ...state,
+        cycles: state.cycles.map((cycle) => {
+          if (cycle.id === state.activeCycleId) {
+            return { ...cycle, finishedDate: new Date() }
+          } else {
+            return cycle
+          }
+        }),
+        activeCycleId: null,
+      }
+    }
+    default: {
+      return state
+    }
+  }
+}
